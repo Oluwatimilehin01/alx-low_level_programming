9f06a13a8212d2e131e8e96d87e9be20af7c5375
Makinde Oluwatimilehin <makindeoluwatimilehin1@gmail.com>
2022-09-09 06:02:37 -0700

    working

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,7 @@
 	 *Return: Always O (success)
 	 */
 int main(void)
-{
-         
-	puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
 
